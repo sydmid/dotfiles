f211b5b50997c98bd32d3491e7a89d10398a9885
veteranomid <seyedomidnajmi@live.com>
2021-06-12 13:59:02 +0430

    visual+functional update ss manage system is fully implemented

--- .scripts/ssmanage/alternate.sh
+++ .scripts/ssmanage/alternate.sh
@@ -1,3 +1,3 @@
-subSession=$(echo "Enter your desired sub-session name" | dmenu) &&
+subSession=$(echo "Enter your desired sub-session name" | rofi -dmenu) &&
 /usr/bin/maim -m 1 -u | /usr/bin/convert - -resize 50% -define jpeg:dct-method=float -quality 80 ~/Pictures/${subSession}_$(date '+%F_%H:%M:%S').jpeg &&
 echo "${subSession}_$(date '+%F_%H:%M:%S').jpeg saved" | xargs -I {} dunstify {}
